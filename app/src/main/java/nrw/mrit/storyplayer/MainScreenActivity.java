9d39e0f2ac8b64e59c650bf98c7b923d086849d6
Marius R <Masemo1234@users.noreply.github.com>
2018-04-23 20:28:41 +0200

    Google signin+sysfunct email sign in TODO ample story

--- app/src/main/java/nrw/mrit/storyplayer/MainScreenActivity.java
+++ app/src/main/java/nrw/mrit/storyplayer/MainScreenActivity.java
@@ -2,6 +2,12 @@ package nrw.mrit.storyplayer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 
 public class MainScreenActivity extends AppCompatActivity {
 
@@ -9,5 +15,11 @@ public class MainScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+
     }
+
+
+
+
 }
